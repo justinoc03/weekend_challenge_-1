56e10c2ccf1766c7568fb26faeb2d2036825c1ea
Justin O'Connor <justin.oc03@gmail.com>
2016-08-28 22:53:34 -0500

    fixed yearly and monthly calculations, still need some CSS

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 console.log('tis working');
 
-var employeeList = [];
+// global variables
+var employees = [];
+var totalSalary = 0;
+var monthlyCost = 0;
 
 var addEmployee = function() {
   console.log('in addEmployee');
 
+//connect with HTML
+  var firstName = document.getElementById('firstNameIn').value;
+  var lastName = document.getElementById('lastNameIn').value;
+  var idNumber = document.getElementById('idNumberIn').value;
+  var jobTitle = document.getElementById('jobTitleIn').value;
+  var annualSalary = document.getElementById('annualSalaryIn').value;
+
   //create employee object
   var newEmpEntry = {
-    firstName: document.getElementById('firstNameIn').value,
-    lastName: document.getElementById('lastNameIn').value,
-    idNumber: document.getElementById('idNumberIn').value,
-    jobTitle: document.getElementById('jobTitleIn').value,
-    annualSalary: document.getElementById('annualSalaryIn').value,
+    firstName: firstName,
+    lastName: lastName,
+    idNumber: idNumber,
+    jobTitle: jobTitle,
+    annualSalary: annualSalary,
   }; // end newEmpEntry
 
   //alert user of blanks (use this if using <button> in html)
@@ -21,35 +31,80 @@ var addEmployee = function() {
   else {
     //call clearInputs
     clearInputs();
-    //push newEmpEntry into the employeeList array
-    employeeList.push(newEmpEntry);
+    //push newEmpEntry into the  array
+    employees.push(newEmpEntry);
     //call displayEmployees
     displayEmployees();
   }// end of error alert
-}; // end of newEmpEntry
+};
 
-//   //push newEmpEntry to employeeList array (use this if using <form> in html)
-//   employeeList.push(newEmpEntry);
+//   //push newEmpEntry to employees array (use this if using <form> in html)
+//   employees.push(newEmpEntry);
 // }; // end of newEmpEntry
 
 var clearInputs = function() {
   console.log('in clearInputs');
-  firstName: document.getElementById('firstNameIn').value = '';
-  lastName: document.getElementById('lastNameIn').value = '';
-  idNumber: document.getElementById('idNumberIn').value = '';
-  jobTitle: document.getElementById('jobTitleIn').value = '';
-  annualSalary: document.getElementById('annualSalaryIn').value = '';
+  document.getElementById('firstNameIn').value = '';
+  document.getElementById('lastNameIn').value = '';
+  document.getElementById('idNumberIn').value = '';
+  document.getElementById('jobTitleIn').value = '';
+  document.getElementById('annualSalaryIn').value = '';
 };
 
 var displayEmployees = function() {
+  totalSalary = 0;
+  monthlyCost = 0;
   console.log('in displayEmployees');
-  console.log(employeeList);
+  console.log(employees);
   // empty our list so we don't repeatedly add already added employees
   document.getElementById('allEmployees').innerHTML='';
   // for each employee, add a list item with all pieces
-  for (var i = 0; i < employeeList.length; i++) {
+  for (var i = 0; i < employees.length; i++) {
     // employee information
-    var employeeInfo = '<h3>Name: ' + employeeList[i].firstName + ' ' + employeeList[i].lastName + '<br>Employee ID #: ' + employeeList[i].idNumber + '<br>Job Title: ' + employeeList[i].jobTitle + '<br>Yearly Salary: $' + employeeList[i].annualSalary + '</h3>';
+    var employeeInfo = '<h3>Name: ' + employees[i].firstName + ' ' + employees[i].lastName + '<br>' +
+      'Employee ID #: ' + employees[i].idNumber + '<br>' +
+      'Job Title: ' + employees[i].jobTitle + '<br>' +
+      'Yearly Salary: $' + employees[i].annualSalary + '</h3>' +
+      '<button onclick="removeEmployee(' + i + ')">Remove Employee</button>';
+
     document.getElementById('allEmployees').innerHTML += employeeInfo;
+
+    document.getElementById('totalSalary').innerHTML += '';
+
+    // calculate total yearly cost
+    totalSalary += Number(employees[i].annualSalary);
+    console.log('The total yearly salary is: $' + totalSalary);
+
+    // calculate monthly cost
+    monthlyCost = Math.round(totalSalary / 12);
+    console.log('The total monthly cost is: $' + monthlyCost);
+
+    // display total monthly Salary
+    document.getElementById('monthlyCostTickerIn').innerHTML = ('Total Monthly Cost: $' + monthlyCost);
+
+
   }
+
 }; //end displayEmployees
+
+var removeEmployee = function(choice) {
+  console.log('in removeEmployee');
+
+  if(choice > -1) {
+    employees.splice(choice, 1);
+
+  displayEmployees();
+  console.log('The total monthly cost is: $' + monthlyCost);
+  }
+};
+
+// var totalMonthlyCost = function() {
+//   console.log('in totalMonthlyCost');
+//   totalSalary = 0; //set as gobal variable in beginning
+//   for (var i = 0; i < employees.length; i++) {
+//     totalSalary += Number(employees[i].annualSalary);
+//
+//     console.log(totalSalary);
+//     return totalSalary;
+//   }
+// };
